86de27c5ead12fdf1cf1ecdf4ee46604a2fc4d18
Siddhant Kumar Patel <silu.patel1997@hotmail.com>
2017-05-07 05:00:33 +0530

    Javadoc comments and variable name refactoring of:
    1. Classes in org.loklak.client package
    2. Classes in org.loklak.tools package
    
    With refference to issue: https://github.com/loklak/loklak_jlib_api/issues/6

--- src/org/loklak/tools/JsonCompatHelper.java
+++ src/org/loklak/tools/JsonCompatHelper.java
@@ -1,28 +1,28 @@
 /**
- *  JsonCompatHelper
- *  Copyright 06.06.2016 by Marc Nause, @nause_marc
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; wo even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program in the file lgpl21.txt
- *  If not, see <http://www.gnu.org/licenses/>.
+ * JsonCompatHelper
+ * Copyright 06.06.2016 by Marc Nause, @nause_marc
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; wo even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program in the file lgpl21.txt
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.loklak.tools;
 
-import java.util.Collection;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.Collection;
 
 /**
  * Provides workaround methods for different JSONObject implementations in the
@@ -30,22 +30,29 @@ import org.json.JSONObject;
  */
 public class JsonCompatHelper {
 
-  private static boolean hasCollectionMethod;
-  static {
-    try {
-      hasCollectionMethod = JSONObject.class.getMethod("put", new Class[] { String.class, Collection.class }) != null;
-    } catch (NoSuchMethodException | SecurityException e) {
-      hasCollectionMethod = false;
-    }
-  }
+    private static final String JSON_OBJECT_METHOD_NAME = "put";
+    private static boolean hasCollectionMethod;
 
-  public static JSONObject put(final JSONObject jsonObject, final String key, final Collection<?> value) {
-
-    if (hasCollectionMethod) {
-      return jsonObject.put(key, value);
-    } else {
-      return jsonObject.put(key, new JSONArray(value));
+    static {
+        try {
+            hasCollectionMethod = JSONObject.class
+                    .getMethod(
+                            JSON_OBJECT_METHOD_NAME,
+                            new Class[]{String.class, Collection.class}) != null;
+        } catch (NoSuchMethodException | SecurityException e) {
+            hasCollectionMethod = false;
+        }
     }
-  }
+
+    public static JSONObject put(
+            final JSONObject jsonObject,
+            final String key,
+            final Collection<?> value) {
+        if (hasCollectionMethod) {
+            return jsonObject.put(key, value);
+        } else {
+            return jsonObject.put(key, new JSONArray(value));
+        }
+    }
 
 }
